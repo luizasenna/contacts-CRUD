bea1e646d34bdfa16d1f4171c56fa5c0ef30d27d
Luiza Senna <wcatkdcnuc@202213.cloudwaysapps.com>
2022-04-21 15:21:57 +0000

    home blade

---
+++ app/Http/Controllers/ContactsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\User;
+use Illuminate\Support\Facades\DB;
+
+
+class ContactsController extends Controller
+{
+
+      public function __construct()
+      {
+        //  $this->middleware('auth');
+      }
+
+      public function home()
+      {
+          return view('/home');
+      }
+
+
+
+
+
+
+  }
---
+++ resources/views/home.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header bg-info text-white">{{ __('Contacts Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    Hello there,  <b></b>
+
+                    <div class="m-3">
+
+                        <table class="table">
+                        <thead>
+                          <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Contact</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Actions</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>Actions</td>
+                          </tr>
+
+
+                        </tbody>
+                      </table>
+                </div>
+
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
